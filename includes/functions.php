4232f282ff684d6f1f71781ba2089a3be2a9de67
Sven Bolte <67223729+svenbolte@users.noreply.github.com>
2024-11-07 21:17:04 +0100

    ago function cleaned

--- includes/functions.php
+++ includes/functions.php
@@ -172,7 +172,7 @@ function etime_menu($selectedmenu,$validuser) {
 	return $mtext;
 }
 
-// Zeitdifferenz ermitteln und gestern/vorgestern/morgen schreiben: *chartscodes, dedo, foldergallery, timeclock
+// Zeitdifferenz ermitteln und gestern/vorgestern/morgen schreiben: chartscodes, dedo, foldergallery, timeclock
 if( !function_exists('ago')) {
 	function ago($timestamp) {
 		if (empty($timestamp)) return;
@@ -180,26 +180,26 @@ if( !function_exists('ago')) {
 		date_default_timezone_set('Europe/Berlin');
 		$now = time();
 		if ($timestamp > $now) {
-			$prepo = __('in', 'penguin');
+			$prepo = 'in';
 			$postpo = '';
 		} else {
 			if ($xlang == 'de') {
-				$prepo = __('vor', 'penguin');
+				$prepo = 'vor';
 				$postpo = '';
 			} else {
 				$prepo = '';
-				$postpo = __('ago', 'penguin');
+				$postpo = __('ago', 'etimeclockwp');
 			}
 		}
 		$her = date( 'd.m.Y', intval($timestamp) );
 		if ($her == date('d.m.Y',$now - (24 * 3600))) {
-			$hdate = __('yesterday', 'penguin');
+			$hdate = __('yesterday', 'etimeclockwp');
 		} else if ($her == date('d.m.Y',$now - (48 * 3600))) {
-			$hdate = __('1 day before yesterday', 'penguin');
+			$hdate = __('1 day before yesterday', 'etimeclockwp');
 		} else if ($her == date('d.m.Y',$now + (24 * 3600))) {
-			$hdate = __('tomorrow', 'penguin');
+			$hdate = __('tomorrow', 'etimeclockwp');
 		} else if ($her == date('d.m.Y',$now + (48 * 3600))) {
-			$hdate = __('1 day after tomorrow', 'penguin');
+			$hdate = __('1 day after tomorrow', 'etimeclockwp');
 		} else {
 			$hdate = ' ' . $prepo . ' ' . human_time_diff(intval($timestamp), $now) . ' ' . $postpo;
 		}
